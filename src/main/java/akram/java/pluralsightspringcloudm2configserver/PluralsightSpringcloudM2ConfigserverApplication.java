3c2041432cb85d4ecc182a3409d27e37424d3968
akrah200 <ak_tv82@yahoo.com>
2020-11-09 00:07:51 -0500

    before changing

---
+++ src/main/java/akram/java/pluralsightspringcloudm2configserver/PluralsightSpringcloudM2ConfigserverApplication.java
@@ -0,0 +1,13 @@
+package akram.java.pluralsightspringcloudm2configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightSpringcloudM2ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightSpringcloudM2ConfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/akram/java/pluralsightspringcloudm2configserver/PluralsightSpringcloudM2ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package akram.java.pluralsightspringcloudm2configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluralsightSpringcloudM2ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
